b93065e3d22d0d67ecd909a4a955a1a718c0c1c1
Jianne Gabriel <jiannegabriel@gmail.com>
2020-12-23 11:57:56 +0800

    initial commit for eCalc

---
+++ eCalc.py
@@ -0,0 +1,17 @@
+# Create a Python project to get the value of e to n number of decimal places.
+print("""This prints the value of e to n number of decimal places""")
+
+from math import e
+
+def eDecimals(x):  #create a function called eDecimals
+    if x > 15 and x < 0:
+        print ("Please choose a number from 0-15")
+    return round(e,x)
+
+try: 
+    decimal = int(input("How many decimal places? n = "))
+    print(eDecimals(decimal))
+except: #catch the error in case of non-integer input
+    print("Please enter an integer")
+
+ 
